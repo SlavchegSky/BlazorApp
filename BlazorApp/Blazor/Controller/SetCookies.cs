de859dd35f0d158093035c9bc1f5a80338f72356
SlavchegSky <slava-sky@yandex.ru>
2020-11-19 21:45:03 +0300

    v1.006

---
+++ BlazorApp/Blazor/Controller/SetCookies.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Controller
+{
+    public class SetCookies
+    {
+        private readonly IHttpContextAccessor _accessor;
+        public SetCookies(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public void CookiesResponse(string key, string value)
+        {
+            var context = _accessor.HttpContext;
+
+            if (context != null)
+            {
+                context.Response.Cookies.Append(key, value);
+            }
+        }
+
+        public async Task<string> CookiesRequest(string key)
+        {
+            var context = _accessor.HttpContext;
+            string result = "нет";
+
+            if (context != null)
+            {
+                 context.Request.Cookies.TryGetValue(key, out result);
+            }
+            await Task.Delay(500);
+            return await Task.FromResult(result);
+        }
+    }
+}
--- BlazorApp/Blazor/Startup.cs
+++ BlazorApp/Blazor/Startup.cs
@@ -26,17 +26,19 @@ namespace Blazor
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddHttpContextAccessor();
+            services.AddScoped<SetCookies>();
             services.AddSingleton<WeatherForecastService>();
             services.AddDbContext<SqlDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("StringSqlConnection")));
             services.AddTransient<IArticleController>();
             services.AddTransient<IAutorController>();
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
         }
 
